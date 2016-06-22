faf7ba22d513da9b54a2cb78de522953c657c101
Keiichiro Shikano <k16.shikano@gmail.com>
2016-06-22 12:50:58 +0900

    debug image tag generation

--- src/EPUB/ImageReader.hs
+++ src/EPUB/ImageReader.hs
@@ -8,6 +8,8 @@ import System.Directory (copyFile, createDirectoryIfMissing)
 import qualified System.FilePath.Posix as FP
 import qualified System.Process as Prc (readProcess)
 
+import qualified Text.XML.HXT.DOM.XmlNode as XN
+import qualified Data.Tree.Class as Tree
 import qualified Debug.Trace as DT (trace)
 
 imagePathToResourceName ::   FilePath      -- input file name
@@ -41,6 +43,8 @@ mkImgId :: FilePath  -> (FilePath, Int) -> String
 mkImgId f (s,c) = 
   FP.dropExtension f ++ FP.takeBaseName s ++ show c
 
+imgAltAttr f (s,c) = sattr "alt" (mkImgId f (s,c+1))
+
 mkImgLead :: FilePath -> String
 mkImgLead f = 
   let htmlpath = FP.takeDirectory f
@@ -49,30 +53,34 @@ mkImgLead f =
 mkImgSrcPath :: String -> String -> FilePath 
 mkImgSrcPath imglead imgid = FP.combine (imglead++"images/png/") $ imgid FP.<.> ".png"
 
+imgSrcAttr f l (s,c) = sattr "src" (mkImgSrcPath l (mkImgId f (s,c)))
+
 imgElem :: (ArrowXml a) => FilePath -> a XmlTree XmlTree
 imgElem f = 
    fromSLA 0 (
-     processBottomUp (
-        (((\(alt,path) -> 
-            eelem "img" 
-            += sattr "src" path
-            += sattr "alt" alt
-            += sattr "class" "figure")
-          $<
-          (getAttrValue "src" &&& nextState (+1) 
-           >>> arr (mkImgId f)
-           >>> (this &&& arr (mkImgSrcPath (mkImgLead f)))))
-         >>> styleAttr)
+     processTopDown (
+        (eelem "img" 
+         += sattr "class" "figure"
+         += (styleAttr $< getAttrValue "width" &&& getAttrValue "height")
+         += (imgAltAttr f $< getAttrValue "src" &&& getState)
+         += (imgSrcAttr f (mkImgLead f) $< getAttrValue "src" &&& nextState (+1)))
         `when`
-        (hasName "img" >>> neg (hasAttrValue "class" (=="inlinemath") <+> hasAttrValue "class" (=="displaymath")))))
+        (hasName "img" >>> 
+         neg (hasAttrValue "class" (=="inlinemath") <+> hasAttrValue "class" (=="displaymath")))))
 
-styleAttr :: (ArrowXml a) => a XmlTree XmlTree
-styleAttr = 
-  ((\(h,w) -> (case (h,w) of
-                  ("","") -> addAttr "style" "width:90%;"
-                  ("",w)  -> addAttr "style" $ "width:"++w++";"
-                  (h,"")  -> addAttr "style" $ "height:"++h++";"
-                  (h,w)   -> addAttr "style" $ "width:"++w++";height:"++h++";"))
-   $<$ ((getAttrValue "height") &&& (getAttrValue "width")))
-  >>> removeAttr "height" >>> removeAttr "width" >>> removeAttr "abovespace"
+styleAttr :: (ArrowXml a) => (String, String) -> a XmlTree XmlTree
+styleAttr (w,h) = (case (w,h) of
+                  ("","") -> sattr "style" "width:90%;"
+                  ("",w')  -> sattr "style" $ "width:"++w'++";"
+                  (h',"")  -> sattr "style" $ "height:"++h'++";"
+                  (h',w')   -> sattr "style" $ "width:"++w'++";height:"++h'++";")
+
+
+-- for test
+tn = XN.mkElement (mkName "img") [ XN.mkAttr (mkName "width") [XN.mkText "10"]
+                                 , XN.mkAttr (mkName "height") [XN.mkText "20"]
+                                 , XN.mkAttr (mkName "src") [XN.mkText "path/to/image.png"]
+                                 ] []
+
+-- runLA (imgElem "foo/bar.html") tn
 
